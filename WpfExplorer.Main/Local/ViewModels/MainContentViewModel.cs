32d8e67d175e78e1c581d21963b02e7659fa8816
김용현 <yhkimjjang@gmail.com>
2024-01-19 22:51:51 +0900

    폴더 트리 뷰 작성

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -1,14 +1,25 @@
-﻿using WpfExplorer.Support.Local.Helpers;
+﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Mvvm;
+using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.Local.ViewModels;
 
-public class MainContentViewModel
+public partial class MainContentViewModel : ObservableBase
 {
+    private readonly FileService _fileService;
+
     public List<FolderInfo> Roots { get; init; }
 
     public MainContentViewModel(FileService fileService)
     {
-        Roots = fileService.GenerateRootNodes();
+        _fileService = fileService;
+        Roots = _fileService.GenerateRootNodes();
+    }
+
+    [RelayCommand]
+    private void FolderChange(FolderInfo? folderInfo)
+    {
+        _fileService.RefreshSubDirectories(folderInfo!);
     }
 }
